de335529d53ea938f81063409c82725b8f1d7374
Conor R <conor.riches@gmail.com>
2021-07-25 22:02:42 +0100

    Sets columns for sso

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -57,7 +57,6 @@ class SessionController extends Controller
 	public function sso()
 	{
         $input = \Input::only('sso', 'sig');
-        \Log::info("Start SSO, Input");
 
         if(empty($input['sso']) || empty($input['sig'])){
             \Log::error("SSO - params not set");
@@ -78,7 +77,6 @@ class SessionController extends Controller
             );
             return \Response::make(json_encode(['success'=>'false']), 200);
         } else {
-            \Log::info("SSO - match hmac");
             /**
              * The sso input is a string, base64 encoded.
              * e.g. "email=test@test.com&password=password"
@@ -87,12 +85,12 @@ class SessionController extends Controller
              */
             parse_str(base64_decode(urldecode($input['sso'])), $parsedInput);
             
-            \Log::info("SSO - pre validate");
-            \Log::info($input['sso']);
             $this->loginForm->validate($parsedInput);
-            \Log::info("SSO - aft validate");
 
-            if (Auth::attempt([$parsedInput['email'], $parsedInput['password']], false)) {
+            if (Auth::attempt([
+                'email': $parsedInput['email'], 
+                'password': $parsedInput['password']
+            ], false)) {
                 \Log::info("SSO - auth attempt okay");
                 
                 /**
@@ -111,7 +109,7 @@ class SessionController extends Controller
                     'username'  => $user->name
                     ]));
                 \Log::info("SSO - user data");
-                \Log::info($userData);
+                \Log::info(var_dump($userData));
                     
                 /**
                  * We need to sign what we return so SSO
