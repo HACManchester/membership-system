87ed0dd95de0c78d41a2d774498d7cb546737132
rossyst <43707667+rossyst@users.noreply.github.com>
2021-07-25 18:37:53 +0100

    Update welcome-admin.php

--- resources/views/emails/welcome-admin.php
+++ resources/views/emails/welcome-admin.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="en-GB">
-<head>
-    <meta charset="utf-8">
-</head>
-<body>
-<h1>New User Alert</h1>
-<h3> {{ $user['given_name'] }} </h3>
-<h3> {{ $user['family_name'] }} </h3>
-<h3> {{ $address['$line_1'] }} </h3>
-<h3> {{ $address['$line_2'] }} </h3>
-<h3> {{ $address['line_3'] }} </h3>
-<h3> {{ $address['line_4'] }} </h3>
-<h3> {{ $address['postcode'] }} </h3>
-</body>
+	<head>
+		<meta charset="utf-8">
+	</head>
+	<body>
+		<h2>New User</h2>
+
+		<div>
+			New User:<br />
+            <h3> {{ $user['given_name'] }}, </h3>
+            <h3> {{ $user['family_name'] }}, </h3>
+            <h3> {{ $address['$line_1'] }}, </h3>
+            <h3> {{ $address['$line_2'] }}, </h3>
+            <h3> {{ $address['line_3'] }}, </h3>
+            <h3> {{ $address['line_4'] }}, </h3>
+            <h3> {{ $address['postcode'] }}, </h3>
+		</div>
+	</body>
 </html>
