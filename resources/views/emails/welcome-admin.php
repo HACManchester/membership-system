4febf1f09aacc1a750380490b842b32f3110ae05
rossyst <43707667+rossyst@users.noreply.github.com>
2021-07-25 18:52:34 +0100

    Update welcome-admin.php

--- resources/views/emails/welcome-admin.php
+++ resources/views/emails/welcome-admin.php
@@ -9,13 +9,18 @@
 		<div>
 			New User:<br />
     
-           <h3> <?php echo  $user['given_name']; ?>" </h3>
-           <h3> <?php echo  $user['family_name']; ?>" </h3>
-           <h3> <?php echo  $user['address']['line_1'] ?>" </h3>
-           <h3> <?php echo  $user['address']['line_2'] ?>" </h3>
-           <h3> <?php echo  $user['address']['line_3'] ?>" </h3>
-           <h3> <?php echo  $user['address']['line_4'] ?>" </h3>
-           <h3> <?php echo  $user['address']['postcode'] ?>" </h3>
+           <h3> <?php echo  $user['given_name']; ?> </h3>
+           <h3> <?php echo  $user['family_name']; ?></h3>
+           <h3> <?php echo  $user['address']['line_1'] ?> </h3>
+           <h3> <?php echo  $user['address']['line_2'] ?></h3>
+           <h3> <?php echo  $user['address']['line_3'] ?> </h3>
+           <h3> <?php echo  $user['address']['line_4'] ?></h3>
+           <h3> <?php echo  $user['address']['postcode'] ?> </h3>
+           <h3> <?php echo  $address['line_1'] ?> </h3>
+           <h3> <?php echo  $address['line_2'] ?></h3>
+           <h3> <?php echo  $address['line_3'] ?> </h3>
+           <h3> <?php echo  $address['line_4'] ?></h3>
+           <h3> <?php echo  $address['postcode'] ?> </h3>
 
 		</div>
 	</body>
