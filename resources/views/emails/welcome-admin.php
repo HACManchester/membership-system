9098940bcfa47c52e0032fba67dce538b681f27a
rossyst <43707667+rossyst@users.noreply.github.com>
2021-07-25 18:23:06 +0100

    Update welcome-admin.php

--- resources/views/emails/welcome-admin.php
+++ resources/views/emails/welcome-admin.php
@@ -4,10 +4,13 @@
     <meta charset="utf-8">
 </head>
 <body>
-<p>{{ $user['given_name'] }} {{ $user['family_name'] }}</p>
-<p>{{ $user['address']['line_1'] }}</p>
-<p>{{ $user['address']['line_2'] }}</p>
-<p>{{ $user['address']['line_3'] }}</p>
-<p>{{ $user['address']['postcode'] }}</p>
+<h1>New User Alert</h1>
+<h3> {{ $user['given_name'] }} </h3>
+<h3> {{ $user['family_name'] }} </h3>
+<h3> {{ $address['$line_1'] }} </h3>
+<h3> {{ $address['$line_2'] }} </h3>
+<h3> {{ $address['line_3'] }} </h3>
+<h3> {{ $address['line_4'] }} </h3>
+<h3> {{ $address['postcode'] }} </h3>
 </body>
 </html>
