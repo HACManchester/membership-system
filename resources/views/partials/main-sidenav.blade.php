d9024cc801026f30c6895b066464f4ca7900836f
Conor R <conor.riches@gmail.com>
2021-08-28 23:12:39 +0100

    unfuckup

--- resources/views/partials/main-sidenav.blade.php
+++ resources/views/partials/main-sidenav.blade.php
@@ -3,11 +3,13 @@
     <header>
         <span class="sidenav-brand">
             <a href="{{ route('home') }}"><img class="" src="/img/logo-new.png" height="100" /></a>
-            @if($user->online_only)
-                <span class="label label-warning">Online Only</span>
-            @else
-                @if (!Auth::guest() && (Auth::user()->status != 'active') )
-                    {!! HTML::statusLabel(Auth::user()->status) !!}
+            @if(!Auth::guest())
+                if($user->online_only)
+                    <span class="label label-warning">Online Only</span>
+                @else
+                    @if (Auth::user()->status != 'active')
+                        {!! HTML::statusLabel(Auth::user()->status) !!}
+                    @endif
                 @endif
             @endif
             
