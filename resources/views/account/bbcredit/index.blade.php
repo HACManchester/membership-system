e6070fc0b53e0eb24927dea2b8e7fa4628fc7720
Conor R <conor.riches@gmail.com>
2021-11-20 22:27:36 +0000

    balance colour

--- resources/views/account/bbcredit/index.blade.php
+++ resources/views/account/bbcredit/index.blade.php
@@ -24,6 +24,7 @@ Hackspace Manchester Balance
         </p>
     </div>
 </div>
+
 <div class="row">
     <div class="col-xs-12 col-sm-3 col-md-3">
         <div class="panel panel-default">
@@ -32,26 +33,24 @@ Hackspace Manchester Balance
             </div>
             <div class="panel-body">
                 <p>Your Current Balance is:</p>
-
                 <div>        
                     <span class="credit-figure" style="{{ $userBalance >= 0 ? 'color:#219724' : 'color:#D9534F' }}">{{ $userBalance }}</span>
                 </div> 
-                <br>
-                <p>
-                    
-                </p>
             </div>
         </div>
     </div>
+</div>
+
+<h3>Add Credit</h3>
 <div class="row">
     <div class="col-xs-12 col-sm-8 col-md-8">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title"><strong>Add Credit</strong></h3>
+                <h3 class="panel-title">Top up with card or cash</h3>
             </div>
             <div class="panel-body">
-                <p>Top Up Using Cash Topup or Direct Debit via this option</p> <br>
-
+                <p>Top Up Using Cash Topup or Direct Debit - select which you'd like from the dropdown.</p> <br>
+                <p>Note: if you don't have a direct debit set up you should pay with cash</p>
                 <div class="paymentModule" data-reason="balance" data-display-reason="Balance Payment" data-button-label="Add Credit" data-methods="gocardless,cash2"></div>
                 <br>
                 <p>
@@ -60,30 +59,31 @@ Hackspace Manchester Balance
             </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Snackspace Expenditure</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="paymentModule" data-reason="snackspace" data-display-reason="Usage Fee" data-button-label="Buy Now" data-methods="balance" data-ref="snackspace"></div>
-                </div>
+</div>
+
+<h3>Log Expenditure</h3>
+<div class="row">
+    <div class="col-sm-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Snackspace Expenditure</h3>
             </div>
-        </div>
-        <div class="col-sm-6">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h3 class="panel-title">Fob Purchase</h3>
-                </div>
-                <div class="panel-body">
-                    <div class="paymentModule" data-reason="Fob" data-display-reason="Usage Fee" data-button-label="Buy Now" data-methods="balance" data-ref="fob"></div>
-                </div>
+            <div class="panel-body">
+                <div class="paymentModule" data-reason="snackspace" data-display-reason="Usage Fee" data-button-label="Buy Now" data-methods="balance" data-ref="snackspace"></div>
             </div>
         </div>
     </div>
-
-       
+    <div class="col-sm-6">
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                <h3 class="panel-title">Fob Purchase</h3>
+            </div>
+            <div class="panel-body">
+                <div class="paymentModule" data-reason="Fob" data-display-reason="Usage Fee" data-button-label="Buy Now" data-methods="balance" data-ref="fob"></div>
+            </div>
+        </div>
+    </div>
+</div>
 
 <div class="row">
     <div class="col-xs-12">
