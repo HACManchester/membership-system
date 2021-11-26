e4f0103906f4bfd49a592badf1f0f97a58ec3005
Conor R <conor.riches@gmail.com>
2021-11-26 23:32:26 +0000

    remove duplicate conf email

--- app/Observer/UserObserver.php
+++ app/Observer/UserObserver.php
@@ -9,7 +9,6 @@ class UserObserver
      * Welcome online only users when they create their account
      */
     public function created($user){
-        $this->sendConfirmationEmail($user);
         
         if($user->online_only) {
             $this->newUser($user);
