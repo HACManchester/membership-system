250286270c716e0b096d60b3bec03209f24d88a1
Conor R <conor.riches@gmail.com>
2021-12-26 18:36:26 +0000

    homepage

--- resources/views/account/partials/get-started.blade.php
+++ resources/views/account/partials/get-started.blade.php
@@ -2,22 +2,26 @@
     <div class="col-xs-12 col-md-12">
         <div class="panel panel-default">
             <div class="panel-heading">
-                <h3 class="panel-title">Getting started...</h3>
+                <h3 class="panel-title">Getting started guide...</h3>
             </div>
             <div class="panel-body">
                 <h4>🚀 Welcome to Hackspace Manchester. Let's get started...</h4>
-                <p>
-                   As a new member, here's some helpful links to get you started!
-                </p>
-                <ul>
+                <ul style="list-style-type: none; padding-left: 20px">
                     <li>
+                        @if ($user->keyFobs()->count() > 0)✅@else🟠@endif 
                         <b>Get an access method</b> - You can <a href="/account/0/edit#access">set up a fob or access code</a> on your account page. 
                     </li>
-                    <li><b>Join in the chat</b> - we have a <a href="https://list.hacman.org.uk" target="_blank">forum</a> and <a href="https://t.me/hacmanchester" target="_blank">Telegram group chat</a></li>
-                    <li><b>Find our common resources</b> - we have a page of <a href="/resources">resources</a> outlining the basics</li>
-                    <li><b>Get your membership induction</b> - all members need to have read the general <a href="/account/0/induction">member induction</a></li>
-                    <li><b>Get trained on equipment</b> - Some equipment requires training, check out the <a href="/equipment">equipment</a> page and request inductions.</li>
-                    <li><b>Get a storage location</b> - every member can <a href="/storage_boxes">claim a storage location</a> subject to the rules.</li>
+                    <li>
+                        💬
+                        <b>Join in the chat</b> - we have a <a href="https://list.hacman.org.uk" target="_blank">forum</a> and <a href="https://t.me/hacmanchester" target="_blank">Telegram group chat</a>
+                    </li>
+                    <li>➡️<b>Find our common resources</b> - we have a page of <a href="/resources">resources</a> outlining the basics</li>
+                    <li>
+                        @if ($user->induction_completed)✅@else🟠@endif 
+                        <b>Get your membership induction</b> - all members need to have read the general <a href="/account/0/induction">member induction</a>
+                    </li>
+                    <li>➡️<b>Get trained on equipment</b> - Some equipment requires training, check out the <a href="/equipment">equipment</a> page and request inductions.</li>
+                    <li>➡️<b>Get a storage location</b> - every member can <a href="/storage_boxes">claim a storage location</a> subject to the rules.</li>
                 </ul>
 
                 <h4>Handy links</h4>
