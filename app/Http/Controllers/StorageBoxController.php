4229a075154994c1f269e9399af715c2cf74a090
Conor R <conor.riches@gmail.com>
2021-12-27 19:54:44 +0000

    show box view

--- app/Http/Controllers/StorageBoxController.php
+++ app/Http/Controllers/StorageBoxController.php
@@ -106,7 +106,7 @@ class StorageBoxController extends Controller
      * @param  int  $id
      * @return Response
      */
-    public function show($id)
+    public function show($boxId)
     {
         $user = \Auth::user();
         $box = $this->storageBoxRepository->getById($boxId);
