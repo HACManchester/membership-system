67e209785122fcac58d1cfdbe167bb44448a0eba
Conor R <conor.riches@gmail.com>
2021-12-30 00:32:17 +0000

    Start SSO login - login route

--- resources/views/session/error.blade.php
+++ resources/views/session/error.blade.php
@@ -13,14 +13,15 @@ Login To Another Service
             <div class="col-xs-12">
                 <h1>⛔</h1>
                 <h3>There has been an error.</h3>
-                <h3>Process aborted.</h3>
                 @if(isset($code))
                     <p>Error code {{ $code }}</p>
 
                     @if($code == '2')
-                        <b>Your email address is not verified. Verify it first before logging into other services.</b>
+                        <b>You cannot continue as your email address is not verified.</b>
+                        <hr/>
+                        <b>Verify it first before logging into other services.</b>
                         <p>To verify it, click "Your Membership" in the left-hand menu, then in the notification, click the link to re-send the verification email</p>
-                        <p>Problems? Email the board or shout out on Telegram</p>
+                        <small>Problems? Email the board or shout out on Telegram</small>
                     @endif
                 @endif
             </div>
