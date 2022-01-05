1a4b227db003604c3999005ee3e9b2be7155cef9
Conor R <conor.riches@gmail.com>
2022-01-05 17:38:14 +0000

    fix error handler

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,13 +46,13 @@ class Handler extends ExceptionHandler {
 
     protected function notifyTelegram($error)
     {
-        $notification = "<b>" . $error->getMessage() . "</b> - " . 
-            $error->getFile() . "" .
-            " - on line " . $error->getLine();
+        $notification = "Message: <b>" . $error->getMessage() . "</b> \n " . 
+            "File: <i>" . $error->getFile() . "</i>\n" .
+            "Line: " . $error->getLine();
         (new HttpClient)->get(
             "https://api.telegram.org/bot" . env('TELEGRAM_BOT_KEY') . "/sendMessage" .
             "?parse_mode=HTML&chat_id=" . env('TELEGRAM_BOT_CHAT') . 
-            "&text=🚨 Exception: " . urlencode($notification)
+            "&text=🚨 <b>Error Thrown</b>\n\n " . urlencode($notification)
         );
     }
 
