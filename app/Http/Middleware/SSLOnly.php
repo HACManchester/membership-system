6ed8a7bda6d937ed8c00a8e50a187bbe9b3febbb
Rob Jackson <rob@rjackson.me>
2022-10-07 17:16:32 +0100

    Fix SSL getting in the way locally

--- app/Http/Middleware/SSLOnly.php
+++ app/Http/Middleware/SSLOnly.php
@@ -1,9 +1,18 @@
 <?php namespace BB\Http\Middleware;
 
 use Closure;
+use Illuminate\Foundation\Application;
 
 class SSLOnly
 {
+    /** @var Application */
+    protected $app;
+
+    public function __construct(Application $app)
+    {
+        $this->app = $app;
+    }
+
     /**
      * Verify the incoming request is via an ssl connection unless its on an approved url
      *
@@ -13,7 +22,8 @@ class SSLOnly
      */
     public function handle($request, Closure $next)
     {
-        if( ! $request->isSecure() && env('FORCE_SECURE', 'true')) {
+        // TODO: Ideally this should be server config instead of app code.
+        if(!$this->app->isLocal() && ! $request->isSecure() && env('FORCE_SECURE', 'true')) {
             if ((strpos($request->path(), 'access-control/') !== 0) && ($request->path() !== 'acs') && ($request->path() !== 'acs/spark')) {
                 return redirect()->secure($request->path());
             }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -11,9 +11,11 @@ class AppServiceProvider extends ServiceProvider {
 	 * @return void
 	 */
 	public function boot()
-	{$this->app['request']->server->set('HTTPS', true);
-		//URL::forceScheme('https');
-		//
+	{
+		if (!$this->app->isLocal()) {
+			// TODO: What's this doing? 🤔
+			$this->app['request']->server->set('HTTPS', true);
+		}
 	}
 
 	/**
