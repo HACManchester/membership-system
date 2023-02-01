6292fb277a010f982271c805aa94c1f16dc8f22d
Rob Jackson <rob@rjackson.me>
2023-02-01 17:35:50 +0000

    [Dashboard] Tweak member status bar padding

--- resources/views/account/partials/member-status-bar.blade.php
+++ resources/views/account/partials/member-status-bar.blade.php
@@ -17,73 +17,74 @@
         </a>
     @endif
 </div>
-<div class="alert alert-info">
-    <div class="btn-group" role="group" aria-label="Basic example">
-        @if($user->online_only)
-            <a href="#" type="button" class="btn btn-warning">Online Only</a>
-        @else
-            <a href="#" type="button" class="btn">
-                {!! HTML::statusLabel($user->status) !!}
-            </a>
-        @endif
-        
-        <a href="#" type="button" class="btn">
-            {!! HTML::spaceAccessLabel($user->active) !!}
+
+<div class="member-status-bar">
+    @if($user->online_only)
+        <div>
+            <span class="label label-warning">Online Only</span>
+        </div>
+    @else
+        <div>
+            {!! HTML::statusLabel($user->status) !!}
+        </div>
+    @endif
+    
+    <div>
+        {!! HTML::spaceAccessLabel($user->active) !!}
+    </div>
+
+    @if (!$user->online_only)
+        <a href="/account/{{ $user->id }}/balance" type="button" class="btn">
+            💰 Balance: {{ $memberBalance }}
         </a>
 
-        @if (!$user->online_only)
-            <a href="/account/{{ $user->id }}/balance" type="button" class="btn">
-            💰 Balance: {{ $memberBalance }}
-            </a>
-
-            @if($user->payment_method)
-            <a href="#" type="button" class="btn">
+        @if($user->payment_method)
+            <div>
                 💳 Subscription: {{ $user->present()->subscriptionDetailLine }}
                 @if ($user->canMemberChangeSubAmount())
                     <small><a href="#" class="js-show-alter-subscription-amount" title="Change Amount">Change</a></small>
                 @endif
-            </a>
-            @endif
+            </div>
+        @endif
 
-                @if ($user->canMemberChangeSubAmount())
-                    <div class="hidden js-alter-subscription-amount-form clearfix">
-                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-payment', $user->id])) !!}
-                        <div class="form-group">
-                            <div class="col-sm-6">
-                                <div class="input-group">
-                                    <div class="input-group-addon">&pound;</div>
-                                    {!! Form::input('number', 'monthly_subscription', round($user->monthly_subscription), ['class' => 'form-control', 'placeholder' => MembershipPayments::getRecommendedPrice() / 100, 'min' => MembershipPayments::getMinimumPrice() / 100, 'step' => '1']) !!}
-                                </div>
-                            </div>
-                            <div class="col-sm-6">
-                                {!! Form::submit('Update', array('class'=>'btn btn-default')) !!}
-                            </div>
+        @if ($user->canMemberChangeSubAmount())
+            <div class="hidden js-alter-subscription-amount-form clearfix">
+                {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-payment', $user->id])) !!}
+                <div class="form-group">
+                    <div class="col-sm-6">
+                        <div class="input-group">
+                            <div class="input-group-addon">&pound;</div>
+                            {!! Form::input('number', 'monthly_subscription', round($user->monthly_subscription), ['class' => 'form-control', 'placeholder' => MembershipPayments::getRecommendedPrice() / 100, 'min' => MembershipPayments::getMinimumPrice() / 100, 'step' => '1']) !!}
                         </div>
-                        {!! Form::close() !!}
-
-                        @if (Auth::user()->isAdmin())
-
-                            @if ($user->payment_method == 'gocardless-variable')
-                                {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
-                                    {!! Form::hidden('payment_method', 'balance') !!}
-                                    {!! Form::submit('Change to balance payment', array('class'=>'btn btn-default')) !!}
-                                    <p>This will try and take their monthly subscription from the members balance</p>
-                                {!! Form::close() !!}
-                            @endif
-
-                            @if ($user->payment_method == 'balance')
-                                {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
-                                {!! Form::hidden('payment_method', 'gocardless-variable') !!}
-                                {!! Form::submit('Change to DD payment', array('class'=>'btn btn-default')) !!}
-                                <p>This switches back to a variable DD or resets the payment method if one doesn't exist</p>
-                                {!! Form::close() !!}
-                            @endif
-
-                        @endif
                     </div>
+                    <div class="col-sm-6">
+                        {!! Form::submit('Update', array('class'=>'btn btn-default')) !!}
+                    </div>
+                </div>
+                {!! Form::close() !!}
+
+                @if (Auth::user()->isAdmin())
+
+                    @if ($user->payment_method == 'gocardless-variable')
+                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
+                            {!! Form::hidden('payment_method', 'balance') !!}
+                            {!! Form::submit('Change to balance payment', array('class'=>'btn btn-default')) !!}
+                            <p>This will try and take their monthly subscription from the members balance</p>
+                        {!! Form::close() !!}
+                    @endif
+
+                    @if ($user->payment_method == 'balance')
+                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
+                        {!! Form::hidden('payment_method', 'gocardless-variable') !!}
+                        {!! Form::submit('Change to DD payment', array('class'=>'btn btn-default')) !!}
+                        <p>This switches back to a variable DD or resets the payment method if one doesn't exist</p>
+                        {!! Form::close() !!}
+                    @endif
+
                 @endif
-            @endif
-    </div>
+            </div>
+        @endif
+    @endif
 </div>
 
 @if($user->gift)
