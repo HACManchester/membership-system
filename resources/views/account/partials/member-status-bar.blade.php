e13fce7ca6c0ff7902cfb8f3f3c0b14b4cf73aa0
Rob Jackson <rob@rjackson.me>
2023-02-01 17:43:23 +0000

    [Dashboard] Use modal to change subscription amount

--- resources/views/account/partials/member-status-bar.blade.php
+++ resources/views/account/partials/member-status-bar.blade.php
@@ -42,46 +42,60 @@
             <div>
                 💳 Subscription: {{ $user->present()->subscriptionDetailLine }}
                 @if ($user->canMemberChangeSubAmount())
-                    <small><a href="#" class="js-show-alter-subscription-amount" title="Change Amount">Change</a></small>
+                    <button type="button" class="btn btn-link btn-xs" data-toggle="modal" data-target="#changeSubscriptionModel">
+                        Change
+                    </button>
                 @endif
             </div>
         @endif
 
         @if ($user->canMemberChangeSubAmount())
-            <div class="hidden js-alter-subscription-amount-form clearfix">
-                {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-payment', $user->id])) !!}
-                <div class="form-group">
-                    <div class="col-sm-6">
-                        <div class="input-group">
-                            <div class="input-group-addon">&pound;</div>
-                            {!! Form::input('number', 'monthly_subscription', round($user->monthly_subscription), ['class' => 'form-control', 'placeholder' => MembershipPayments::getRecommendedPrice() / 100, 'min' => MembershipPayments::getMinimumPrice() / 100, 'step' => '1']) !!}
+            <div class="modal fade" id="changeSubscriptionModel" tabindex="-1" role="dialog" aria-labelledby="changeSubscriptionModelLabel">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title" id="changeSubscriptionModelLabel">Change subscription amount</h4>
+                        </div>
+                        <div class="modal-body">
+                            {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-payment', $user->id])) !!}
+                                <div class="form-group">
+                                    <div class="col-sm-6">
+                                        <div class="input-group">
+                                            <div class="input-group-addon">&pound;</div>
+                                            {!! Form::input('number', 'monthly_subscription', round($user->monthly_subscription), ['class' => 'form-control', 'placeholder' => MembershipPayments::getRecommendedPrice() / 100, 'min' => MembershipPayments::getMinimumPrice() / 100, 'step' => '1']) !!}
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-6">
+                                        {!! Form::submit('Update', array('class'=>'btn btn-default')) !!}
+                                    </div>
+                                </div>
+                            {!! Form::close() !!}
+
+                            
+                            @if (Auth::user()->isAdmin())
+                                <div>
+                                    <h2>Admin options</h2>
+                                    @if ($user->payment_method == 'gocardless-variable')
+                                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
+                                            {!! Form::hidden('payment_method', 'balance') !!}
+                                            {!! Form::submit('Change to balance payment', array('class'=>'btn btn-default')) !!}
+                                            <p>This will try and take their monthly subscription from the members balance</p>
+                                        {!! Form::close() !!}
+                                    @endif
+
+                                    @if ($user->payment_method == 'balance')
+                                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
+                                        {!! Form::hidden('payment_method', 'gocardless-variable') !!}
+                                        {!! Form::submit('Change to DD payment', array('class'=>'btn btn-default')) !!}
+                                        <p>This switches back to a variable DD or resets the payment method if one doesn't exist</p>
+                                        {!! Form::close() !!}
+                                    @endif
+                                </div>
+                            @endif
                         </div>
                     </div>
-                    <div class="col-sm-6">
-                        {!! Form::submit('Update', array('class'=>'btn btn-default')) !!}
-                    </div>
                 </div>
-                {!! Form::close() !!}
-
-                @if (Auth::user()->isAdmin())
-
-                    @if ($user->payment_method == 'gocardless-variable')
-                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
-                            {!! Form::hidden('payment_method', 'balance') !!}
-                            {!! Form::submit('Change to balance payment', array('class'=>'btn btn-default')) !!}
-                            <p>This will try and take their monthly subscription from the members balance</p>
-                        {!! Form::close() !!}
-                    @endif
-
-                    @if ($user->payment_method == 'balance')
-                        {!! Form::open(array('method'=>'POST', 'class'=>'', 'style'=>'margin-bottom:20px;', 'route' => ['account.update-sub-method', $user->id])) !!}
-                        {!! Form::hidden('payment_method', 'gocardless-variable') !!}
-                        {!! Form::submit('Change to DD payment', array('class'=>'btn btn-default')) !!}
-                        <p>This switches back to a variable DD or resets the payment method if one doesn't exist</p>
-                        {!! Form::close() !!}
-                    @endif
-
-                @endif
             </div>
         @endif
     @endif
