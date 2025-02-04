c70ce4a0313e3fc78567858c3b8aa6737aef3be6
Rob Jackson <rob@rjackson.me>
2025-02-04 21:13:53 +0000

    Hide online member storage claims

--- resources/views/account/partials/signposts/item-storage.blade.php
+++ resources/views/account/partials/signposts/item-storage.blade.php
@@ -1,18 +1,30 @@
 <li class="signposts-grid-item">
-    <div class="panel panel-default">
+    <div class="panel {{ $user->storageBoxes()->count() ? 'panel-danger' : 'panel-warning' }}">
         <div class="panel-heading">
             <h3 class="panel-title">📦 Storage space</h3>
         </div>
         <div class="panel-body">
-            <p><strong>Your storage space is: {{ $user->storageBoxes()->count() ? $user->storageBoxes()->first()->location : '(none claimed)' }}</strong></p>
+            @if ($user->storageBoxes()->count() > 0)
+                <div class="alert alert-danger">
+                    <strong>Storage is changing (action required)</strong>
+                    <p>Please add a sticker to any items you've stored before 1st July 2025, or your items may be disposed of.</p>
+                    <p>See signage in the Hackspace storage area for full details.</p>
+                </div>
+                <p><strong>Your storage space is: {{ $user->storageBoxes()->count() ? $user->storageBoxes()->first()->location : '(none claimed)' }}</strong></p>
 
-            <p>To claim a storage space, or return your space for somebody else to use, visit the Member&apos;s Storage page.</p>
+                <p>To relinquish your claimed storage location, visit the Member&apos;s Storage page.</p>
 
-            <div class="signpost-grid-item-buttons">
-                <a class="btn btn-primary btn-block" href="{{ route('storage_boxes.index') }}">
-                    Manage Storage
-                </a>
-            </div>
+                <div class="signpost-grid-item-buttons">
+                    <a class="btn btn-primary btn-block" href="{{ route('storage_boxes.index') }}">
+                        Manage Storage
+                    </a>
+                </div>
+            @else
+                <p>
+                    As of January 2025, storage is managed physically with time-limited stickers.
+                    See signage in the Hackspace storage area for full details.
+                </p>
+            @endif
         </div>
     </div>
 </li>
