d06b57c7843bbbfa6bdcdda433f06be092bce36c
Rob Jackson <rob@rjackson.me>
2025-07-22 00:54:11 +0100

    Fix new nav spacing on mobile

--- resources/js/Components/TopNav.tsx
+++ resources/js/Components/TopNav.tsx
@@ -4,34 +4,36 @@ import {
     Link,
     Paper,
     useMediaQuery,
-    Theme
-} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+    Theme,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 type AuthUser = {
     name: string;
     account_path: string;
-}
+};
 
 type Auth = {
     user: AuthUser;
-}
+};
 
 type TopNavProps = {
     handleDrawerToggle: () => void;
     auth: Auth | null;
-}
+};
 
 export default function TopNav({ handleDrawerToggle, auth }: TopNavProps) {
-    const isMobile = useMediaQuery((theme: Theme) => theme.breakpoints.down('md'));
+    const isMobile = useMediaQuery((theme: Theme) =>
+        theme.breakpoints.down("md")
+    );
 
     return (
         <Paper
             square
             elevation={0}
             sx={{
-                borderBottom: '1px solid black',
-                zIndex: (theme) => theme.zIndex.drawer + 1
+                borderBottom: "1px solid black",
+                zIndex: (theme) => theme.zIndex.drawer + 1,
             }}
         >
             <Stack
@@ -41,30 +43,72 @@ export default function TopNav({ handleDrawerToggle, auth }: TopNavProps) {
                 spacing={2}
                 p={2}
             >
-                <Stack direction="row" spacing={2} alignItems="center">
+                <Stack
+                    direction="row"
+                    spacing={isMobile ? 1 : 2}
+                    alignItems="center"
+                    sx={{ overflow: "hidden" }}
+                >
                     {isMobile && (
                         <IconButton
                             color="inherit"
                             aria-label="open drawer"
                             edge="start"
                             onClick={handleDrawerToggle}
+                            sx={{ mr: 0 }}
                         >
                             <MenuIcon />
                         </IconButton>
                     )}
 
-                    <Link href="https://hacman.org.uk" target="_blank" rel="noopener">Website</Link>
-                    <Link href="https://list.hacman.org.uk" target="_blank" rel="noopener">Forum</Link>
-                    <Link href="https://docs.hacman.org.uk" target="_blank" rel="noopener">Documentation</Link>
+                    <Link
+                        href="https://hacman.org.uk"
+                        target="_blank"
+                        rel="noopener"
+                        sx={{
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            minWidth: 0,
+                        }}
+                    >
+                        Website
+                    </Link>
+                    <Link
+                        href="https://list.hacman.org.uk"
+                        target="_blank"
+                        rel="noopener"
+                        sx={{
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            minWidth: 0,
+                        }}
+                    >
+                        Forum
+                    </Link>
+                    <Link
+                        href="https://docs.hacman.org.uk"
+                        target="_blank"
+                        rel="noopener"
+                        sx={{
+                            whiteSpace: "nowrap",
+                            overflow: "hidden",
+                            textOverflow: "ellipsis",
+                            minWidth: 0,
+                        }}
+                    >
+                        Documentation
+                    </Link>
                 </Stack>
 
-                <Stack direction="row" spacing={2} alignItems="center">
+                <Stack direction={isMobile ? "column" : "row"} spacing={isMobile ? 0 : 2} alignItems="center" justifyContent="center">
                     {auth ? (
                         <>
                             <Link href={auth.user.account_path}>
                                 {auth.user.name}
                             </Link>
-                            <Link href="/logout">🔑 Logout</Link>
+                            <Link href="/logout" fontSize="small">Logout</Link>
                         </>
                     ) : (
                         <>
