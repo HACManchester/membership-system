ac65579b91783f913059905f98f22e7980701d13
Rob Jackson <rob@rjackson.me>
2025-08-03 15:00:43 +0100

    Show sidebar badges on old nav

--- resources/views/partials/components/side-nav-link.blade.php
+++ resources/views/partials/components/side-nav-link.blade.php
@@ -1,5 +1,12 @@
 @php
-$style = $highlight ?? false ? "background:yellow;" : "";
+    $style = $highlight ?? false ? 'background:yellow;' : '';
 @endphp
 
-<li><a style="{{ $style }}" href="{{ $href }}">{{ $name }}</a></li>
+<li>
+    <a style="{{ $style }}" href="{{ $href }}">
+        <span>{{ $name }}</span>
+        @if ($badge)
+            <span class="badge">{{ $badge }}</span>
+        @endif
+    </a>
+</li>
